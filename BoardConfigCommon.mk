9fcfa45a08a9b6e2dd22efe701b5dee88cca6895
Shreps <shr3ps@gmail.com>
2014-11-17 08:32:01 +0100

    SELinux commented for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,29 +84,31 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.t30
 BOARD_HARDWARE_CLASS := device/acer/t30-common/cmhw/
 
 # Selinux
-BOARD_SEPOLICY_DIRS += \
-	device/acer/t30-common/sepolicy
+FORCE_PERMISSIVE_TO_UNCONFINED := false
 
-BOARD_SEPOLICY_UNION += \
-        file_contexts \
-        genfs_contexts \
-        bluetooth.te \
-        device.te \
-        domain.te \
-        drmserver.te \
-        init_shell.te \
-        file.te \
-        gpsd.te \
-        keystore.te \
-        lmkd.te \
-        mediaserver.te \
-        rild.te \
-        sensors_config.te \
-        surfaceflinger.te \
-        system_app.te \
-        system_server.te \
-        ueventd.te \
-        vold.te
+#BOARD_SEPOLICY_DIRS += \
+#	device/acer/t30-common/sepolicy
+
+#BOARD_SEPOLICY_UNION += \
+#        file_contexts \
+#        genfs_contexts \
+#        bluetooth.te \
+#        device.te \
+#        domain.te \
+#        drmserver.te \
+#        init_shell.te \
+#        file.te \
+#        gpsd.te \
+#        keystore.te \
+#        lmkd.te \
+#        mediaserver.te \
+#        rild.te \
+#        sensors_config.te \
+#        surfaceflinger.te \
+#        system_app.te \
+#        system_server.te \
+#        ueventd.te \
+#        vold.te
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/acer/t30-common/prebuilt/ramdisk/fstab.acer
